1582a8d0c4ddab2263e1781ee3db0e1ee093efc1
Derrik Milligan <37845846+DerrikMilligan@users.noreply.github.com>
2022-01-27 20:24:30 -0700

    Finished team activity

--- week4/modules/tic_tac_toe.mjs
+++ week4/modules/tic_tac_toe.mjs
@@ -2,10 +2,24 @@
 export class TicTacToe {
 
   constructor(cellSelector, resetSelector) {
+    this.board = [];
     this.cellSelector = cellSelector;
-    document.querySelector(resetSelector).addEventListener('click', this.resetGame);
+
+    document.querySelector(resetSelector).addEventListener('click', this.resetGame.bind(this));
     this.resetGame();
-    console.log(this.board);
+  }
+
+  removeOldListeners() {
+    // wipe out all the old events before making new ones
+    this.board.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell && cell.listener) {
+          [ 'ontouchend', 'click' ].forEach((event) => {
+            cell.el.removeEventListener(event, cell.listener);
+          });
+        }
+      });
+    });
   }
 
   initializeBoard() {
@@ -15,6 +29,8 @@ export class TicTacToe {
       alert('Hey this board has an incorrect number of cells');
     }
 
+    this.removeOldListeners();
+
     this.board = [];
 
     let row = [];
@@ -22,18 +38,17 @@ export class TicTacToe {
       const cell = {
         el: element,
         value: '',
+        listener: null,
       };
+
+      cell.listener = this.clickCell.bind(this, cell);
+
+      cell.el.children[0].innerText = '';
+
       row.push(cell);
 
-      const clickCallback = () => {
-        cell.value = this.currentPlayer;
-        cell.el.children[0].innerText = cell.value;
-        this.switchPlayer();
-      };
-
       [ 'ontouchend', 'click' ].forEach((event) => {
-        element.removeEventListener(event, clickCallback);
-        element.addEventListener(event, clickCallback);
+        element.addEventListener(event, cell.listener);
       });
 
       if ((index + 1) % 3 === 0) {
@@ -44,19 +59,107 @@ export class TicTacToe {
   }
 
 
-  clickCell(event) {
+  clickCell(cell, event) {
+    cell.value = this.currentPlayer;
+    cell.el.children[0].innerText = cell.value;
 
-    if (!this.checkForWinner()) {
-      this.switchPlayer();
-    }
-  }
-
-  getGameState() {
-
+    // We wait a little to actually see the letter before checking for a winner
+    setTimeout(() => {
+      if (!this.checkForWinner()) {
+        this.switchPlayer();
+      }
+    }, 100);
   }
 
   checkForWinner() {
-    return true;
+    // Check for horizontal wins
+    for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+      const row = this.board[rowIndex];
+
+      let win = true;
+      let player = row[0].value;
+      for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+        const cell = row[columnIndex];
+
+        if (cell.value !== player) {
+          win = false;
+        }
+      }
+
+      if (win && player !== '') {
+        alert("There's a horizontal win!");
+        return true;
+      }
+    }
+
+    // Check for vertical wins
+    for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+      let win = true;
+      let player = this.board[0][columnIndex].value;
+
+      for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+        const cell = this.board[rowIndex][columnIndex];
+
+        if (cell.value !== player) {
+          win = false;
+        }
+      }
+
+      if (win && player !== '') {
+        alert("There's a vertical win!");
+        return true;
+      }
+    }
+
+    // Check for diagonal wins
+    let win = true;
+    let player = this.board[0][0].value;
+    for (let i = 0; i < 3; i++) {
+      const cell = this.board[i][i];
+
+      if (cell.value !== player) {
+        win = false;
+      }
+    }
+
+    if (win && player !== '') {
+      alert("There's a left diagonal win!");
+      return true;
+    }
+
+    win = true;
+    player = this.board[0][2].value;
+    for (let i = 0; i < 3; i++) {
+      const cell = this.board[i][2 - i];
+
+      if (cell.value !== player) {
+        win = false;
+      }
+    }
+
+    if (win && player !== '') {
+      alert("There's a right diagonal win!");
+      return true;
+    }
+
+    // Check for a draw
+    let draw = true;
+    for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+      for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+        const cell = this.board[rowIndex][columnIndex];
+
+        if (cell.value === '') {
+          draw = false;
+        }
+      }
+    }
+
+    if (draw) {
+      alert('You had a draw. Click reset board to play again.');
+      return true;
+    }
+
+    return false;
   }
 
   switchPlayer() {
